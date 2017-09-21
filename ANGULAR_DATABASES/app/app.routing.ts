4a855d516c2254ae49c05e89bd8a40f6924b3752
I.A <Takler21@users.noreply.github.com>
2017-09-21 15:22:02 +0200

    Add files via upload

---
+++ ANGULAR_DATABASES/app/app.routing.ts
@@ -0,0 +1,10 @@
+﻿import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './components/user.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: 'user', pathMatch: 'full' },
+    { path: 'user', component: UserComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
